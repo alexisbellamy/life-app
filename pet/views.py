3f445431d8f095c185258804fd8cdb101d4de62b
alexisbellamy <129547537+alexisbellamy@users.noreply.github.com>
2023-04-03 22:06:59 +0200

    Update views.py

--- pet/views.py
+++ pet/views.py
@@ -8,14 +8,20 @@ import datetime
 from pet.models import Pet, PetNeeds, PetFeeds
 
 def get_last_needs():
+
     data = {}
     lastneeds = PetNeeds.objects.filter(done=True).order_by('-date_out').first()
     if lastneeds:
+        time = datatime.datetime.now() - lastneeds.date_out.replace(tzinfo=None)
+        time_hour = '{:02d}'.format(int(time.seconds / 60 / 24))
+        time_minute = '{:02d}'.format(int((time.seconds / 60) % 60 ))
+        long_time = time_hour + 'heure(s) et' + time_minute + ' minute(s)'
         data = {
             "poop": lastneeds.poop,
             "pee": lastneeds.pee,
             "walk": lastneeds.walk,
-            "time": lastneeds.delta
+            "time": lastneeds.delta,
+            "long_time": long_time
         }
     return data
 
